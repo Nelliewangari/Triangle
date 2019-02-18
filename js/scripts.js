fc056a670ecd059959c1b70cb86ee5267e71d937
Nellie Wangari <Nelliewangari11@gmail.com>
2019-02-18 10:20:29 +0300

    add js styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,11 @@
 if (isNaN(sideOne) || isNaN(sideTwo) || isNaN(sideThree)) {
   alert("all sides must be numerical");
 } else if (sideOne >= (sideTwo + sideThree) || sideTwo >= (sideThree + sideOne) || sideThree >= (sideOne + sideTwo)) {
-  alert("This is not a valid triangle");
+  alert("This is not triangle");
 } else if (sideOne===sideTwo && sideTwo===sideThree) {
-  return "equilateral";
+  alert"equilateral";
 } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo) {
-  return "isosceles";
+  alert "isosceles";
 } else if (sideOne!=sideTwo && sideOne!=sideThree) {
-  return "scalene";
-if (sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)) {
-  if (sideOne===sideTwo && sideTwo===sideThree) {
-    return "equilateral";
-  } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo) {
-    return "isosceles";
-  } else {
-    return "scalene";
-  }
-} else {
-  return sides;
-  return "These sides do not a triangle make.";
-}
+  alert"scalene";
 };
